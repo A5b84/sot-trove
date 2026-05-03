203f6cab35607ecc316cc762e27d4ac058f530f4
A5b84 <42934711+A5b84@users.noreply.github.com>
2026-05-03 19:13:14 +0200

    Add GitHub action to redeploy

--- packages/extractor/src/index.ts
+++ packages/extractor/src/index.ts
@@ -6,6 +6,10 @@ import { WikitextTreasureProvider } from './treasure/WikitextTreasureProvider';
 const treasureProvider = new WikitextTreasureProvider();
 const treasures = await treasureProvider.getTreasures();
 
+if (treasures.length === 0) {
+    throw new Error('Did not retrieve any treasure, something is probably broken');
+}
+
 const gameData: GameData = {
     generatedOn: Date.now(),
     treasures,
